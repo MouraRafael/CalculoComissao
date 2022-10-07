fee438b58605b5147b4cf4f64bb65e166856ea82
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-07 13:35:55 -0300

    feat: calculo da comissao

--- src/app/vendedor/vendedor.component.ts
+++ src/app/vendedor/vendedor.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendedor.component.reset.css','./vendedor.component.css']
 })
 export class VendedorComponent implements OnInit {
-
+  data={
+    salario:0,
+    comissaoVenda:0,
+    qtdCarrosVendidos:0,
+    comissaoTotal: 0
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  atualizaSalario(sal:string){
+    let num = parseFloat(sal);
+    this.data.salario = num;
+  }
+  atualizaComissao(comi:string){
+    let num = parseFloat(comi)
+    this.data.comissaoVenda = num;
+  }
+  
+  
+  atualizaCarros(qtd:string){
+    let num = parseInt(qtd)
+    this.data.qtdCarrosVendidos = num;
+  }
+
+  calcula(){
+    this.data.comissaoTotal = this.data.salario+(this.data.comissaoVenda*this.data.qtdCarrosVendidos);
   }
 
 }
